ccf6a40c5dcc02e55a58b2891eac9caf8e6af839
huankong <2669868310@qq.com>
2019-03-15 17:45:20 +0800

    添加redis缓存功能

---
+++ src/main/java/com/huankong/fictionalfiction/bean/BookRequestBody.java
@@ -0,0 +1,13 @@
+package com.huankong.fictionalfiction.bean;
+
+public class BookRequestBody {
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
--- src/main/java/com/huankong/fictionalfiction/controller/BookChapterController.java
+++ src/main/java/com/huankong/fictionalfiction/controller/BookChapterController.java
@@ -2,24 +2,38 @@ package com.huankong.fictionalfiction.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.huankong.fictionalfiction.bean.BookRequestBody;
 import com.huankong.fictionalfiction.bean.chapter.BookChapter;
+import com.huankong.fictionalfiction.bean.content.BookContent;
 import com.huankong.fictionalfiction.service.BookChapterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class BookChapterController {
     @Autowired
     BookChapterService bookChapterService;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+    private BookChapter bookChapter;
 
     @GetMapping(value = "/fictionalfiction/bookchapter")
-    public BookChapter bookChapter(@RequestBody String requestString) {
-        Map<String, String> map = new Gson().fromJson(requestString, new TypeToken<HashMap>(){}.getType());
-        return bookChapterService.bookChapter(map.get("url"));
+    public BookChapter bookChapter(@RequestBody BookRequestBody bookRequestBody) {
+        String responseString = stringRedisTemplate.opsForValue().get(bookRequestBody.getKey());
+        if (responseString != null && responseString.length() > 0) {
+            return new Gson().fromJson(responseString, new TypeToken<BookChapter>() {
+            }.getType());
+        } else {
+            bookChapter = bookChapterService.bookChapter(bookRequestBody.getKey());
+            stringRedisTemplate.opsForValue().set(bookRequestBody.getKey(), new Gson().toJson(bookChapter), 5 * 60, TimeUnit.SECONDS);
+            return bookChapter;
+        }
     }
 }
